8dfabad7eae03c3fd934603789735e28f9f5987c
DearMad <31157189+DearMad@users.noreply.github.com>
2017-08-24 14:04:59 +0430

    Update installer.rb

--- installer.rb
+++ installer.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/ruby
 puts "\nTo run the program, we need to install the listed tools :"
-puts "\n\t sl , gem , cowsay , bsdgames , colorize, mplayer\n"
+puts "\n\t sl , gem , cowsay , bsdgames , colorize, mplayer, curl, banner, figlet\n"
+puts "\nPlease Waiting .... (You have 5 seconds to cancel the program)\n"
+system "sleep 8"
+puts "\nOky LetsGo!"
 puts ""
-system "sudo apt update ; sudo apt install gem sl cowsay bsdgames mplayer"
+system "sudo apt update ; sudo apt install gem sl cowsay bsdgames mplayer curl sysvbanner figlet"
 system "sudo gem install colorize"
