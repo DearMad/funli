386c3ddca7323ff0a36944fc39bbd3c5172e80bc
DearMad <31157189+DearMad@users.noreply.github.com>
2017-08-24 15:48:37 +0430

    Delete installer.rb

--- installer.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-puts "\nTo run the program, we need to install the listed tools :"
-puts "\n\t sl , gem , cowsay , bsdgames , colorize, mplayer, curl, banner, figlet\n"
-puts "\nPlease Waiting .... (You have 5 seconds to cancel the program)\n"
-system "sleep 8"
-puts "\nOky LetsGo!"
-puts ""
-system "sudo apt update ; sudo apt install gem sl cowsay bsdgames mplayer curl sysvbanner figlet"
-system "sudo gem install colorize"
