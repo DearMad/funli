f0913bc99d7b2122f19efe46c0b9c33abeda053a
DearMad <31157189+DearMad@users.noreply.github.com>
2017-08-24 15:48:27 +0430

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+pacmngdl(){
+	sudo apt update
+	sudo apt install ruby sl gem cowsay bsdgames mplayer curl figlet
+	sleep 3
+	clear
+}
+clear
+echo -e """
+To run the program, we need to install the listed tools :
+
+\truby, sl , gem , cowsay , bsdgames , colorize, mplayer, curl, banner, figlet
+
+"""
+id | grep sudo  > /dev/null
+sudo=$?
+chmod +x funli
+pacmngdl
+checkdl=$?
+if [ "$checkdl" = "0" ];then
+	echo -e "\t\tbarname ha nasb shodan!!! [IR]"
+	echo "Hala Barname bayad be dastorat ezafe beshe!![IR]"
+	if [ "$sudo" = "0" ];then  #If Its == Ture , You Are Sudo
+		sudo cp funli /bin
+		sudo chmod +x /bin/rcode
+	else 
+		PATH=$PATH:$PWD
+		export PATH
+		echo "$PATH" >> ~/.bashrc
+	fi
+	echo "Khob Barname ba movafaghiat Nasb shod!!!"
+	echo "Harvaght ke ba barname kar dashtin az dastoor 'funli' Mitonin estefade konid! [IR]"
+else
+	if [ "$checkdl" = "100" ] ; then
+		echo "Motmaen Bashid Ke internet shoma vasle va makhazen shoma update hast [IR]"
+		exit
+	else
+		echo "Unknow ERROR!!!!"
+		echo "shoma bayad aval barname hara nasb konid va sepas funli dar dastoorat ezafe konid va sepas funli ro farakhani bekonid [IR]"
+
+	fi
+fi
+echo "Movaffagh bashid!!!"
+## Github.com/dearmad/funli
